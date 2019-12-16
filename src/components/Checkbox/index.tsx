d91029cd61395f49e18a92f4721ed35a6d8984d5
Franco Valdes <franco.valdes89@gmail.com>
2019-12-16 17:31:27 -0500

    checkbox rework

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -11,26 +11,6 @@ interface ICheckboxProps
     onChange?: (e: any) => void;
   };
 
-// class Checkbox extends React.Component<ICheckboxProps> {
-//   public render() {
-//     const { id, name, value, defaultChecked, disabled, variant = 'primary', checkedColor = 'primary', checkboxSize = 'small' } = this.props;
-
-//     return (
-//       <StyledCheckbox
-//         type="checkbox"
-//         id={id}
-//         name={name}
-//         value={value}
-//         defaultChecked={defaultChecked}
-//         disabled={disabled}
-//         checkedColor={checkedColor}
-//         variant={variant}
-//         checkboxSize={checkboxSize}
-//       />
-//     )
-//   }
-// }
-
 const Checkbox: React.FC<ICheckboxProps> = ({
   boxSize = 'medium',
   defaultChecked,
