981bbede64eb061ed95155d0415eb2e762e186e5
Chris Weissenberger <chris@Chris-Weissenberger.local>
2019-12-17 12:03:56 -0500

    Pagination Storybook knobs

--- src/components/Pagination/pagination.stories.tsx
+++ src/components/Pagination/pagination.stories.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { number } from '@storybook/addon-knobs';
 import { ThemeProvider } from '../../index';
 import { BaseStyles } from '../BaseStyles';
 import { Pagination } from './index';
 
 storiesOf("Pagination", module)
-  .add("Default", () => (
+  .add("Default", () => {
+    const totalItems = number('Total Items', 150);
+    const maxButtons = number('Max Center Buttons', 3);
+    const perPage = number('Items Per Page', 10);
+    return (
     <ThemeProvider>
       <BaseStyles />
-      <Pagination totalItems={180} maxButtons={4} perPage={50} />
+      <Pagination totalItems={totalItems} maxButtons={maxButtons} perPage={perPage} />
     </ThemeProvider>
-  ));
+  )});
